92218475985436c019cca4b077542be09a76d55a
kid2Ion <hiroki.kondo@zuuonline.com>
2022-10-16 16:33:24 +0900

    client test

--- repo/main.go
+++ repo/main.go
@@ -1,29 +1,79 @@
 package main
 
 import (
-	"article_GraphQL_gRPC/repo/injector"
+	"article_GraphQL_gRPC/repo/client"
 	"article_GraphQL_gRPC/repo/pb"
+	"context"
+	"fmt"
+	"io"
 	"log"
-	"net"
-
-	"google.golang.org/grpc"
 )
 
+// gRPCサーバー動作確認用
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	c, _ := client.NewClient("localhost:50051")
+	list(c)
+}
+
+func create(c *client.Client) {
+	input := &pb.ArticleInput{
+		Author:  "著者1",
+		Title:   "タイトル1",
+		Content: "内容1",
+	}
+	res, err := c.Service.CreateArticle(context.Background(), &pb.CreateArticleRequest{ArticleInput: input})
 	if err != nil {
-		log.Fatal("failed to listen: %v\n", err)
+		log.Fatalf("Failed to CreateArticle: %v\n", err)
 	}
-	defer lis.Close()
+	fmt.Printf("CreateArticles response: %v\n", res)
+}
 
-	service := injector.InjectArticleService()
-	server := grpc.NewServer()
+func read(c *client.Client) {
+	var id int64 = 1
+	res, err := c.Service.ReadArticle(context.Background(), &pb.ReadArticleRequest{Id: id})
+	if err != nil {
+		log.Fatalf("fatal to read article: %v\n", err)
+	}
+	fmt.Printf("ReadArticle response: %v\n", res)
+}
 
-	pb.RegisterArticleServiceServer(server, service)
+func update(c *client.Client) {
+	var id int64 = 1
+	input := &pb.ArticleInput{
+		Author:  "著者1new",
+		Title:   "タイトル1new",
+		Content: "内容1new",
+	}
+	res, err := c.Service.UpdateArticle(context.Background(), &pb.UpdateArticleRequest{Id: id, ArticleInput: input})
+	if err != nil {
+		log.Fatalf("fatal to update article: %v\n", err)
+	}
+	fmt.Printf("updateArticle response: %v\n", res)
+}
 
-	log.Println("Listening on port 50051...")
-	if err := server.Serve(lis); err != nil {
-		log.Fatal("failed to serve: %v", err)
+func delete(c *client.Client) {
+	var id int64 = 2
+	res, err := c.Service.DeleteArticle(context.Background(), &pb.DeleteArticleRequest{Id: id})
+	if err != nil {
+		log.Fatalf("failed to delete article: %v\n", err)
+	}
+	fmt.Printf("DeleteArticle response: %v\n", res)
+}
+
+func list(c *client.Client) {
+	stream, err := c.Service.ListArticle(context.Background(), &pb.ListArticleRequest{})
+	if err != nil {
+		log.Fatalf("failed to listArticle: %v\n", err)
 	}
 
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("failed to server streaming: %v\n", err)
+		}
+		fmt.Println(res)
+	}
 }
